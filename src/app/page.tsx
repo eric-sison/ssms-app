3c2584401bc9002ca3d3871c009b25bf886f05cc
Eric Sison <ericsison.dev@gmail.com>
2024-01-04 16:49:50 +0800

    minor dev changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,5 @@
-import { Button } from "@/components/ui/Button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/Dialog";
+import React from "react";
 
 export default function HomePage() {
-  return (
-    <main className="h-screen w-screen flex items-center justify-center">
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant={"secondary"}>Open Dialog</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader className="space-y-4">
-            <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </main>
-  );
+  return <main className="h-screen w-screen flex items-center justify-center">Test</main>;
 }
