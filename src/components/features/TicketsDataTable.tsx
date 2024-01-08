604e67fbf48b29989156ec2930dd71391d973bf2
Eric Sison <ericsison@Erics-Mac-mini.local>
2024-01-09 00:02:14 +0800

    added filterFn to fix issue on filterValue

--- src/components/features/TicketsDataTable.tsx
+++ src/components/features/TicketsDataTable.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { ColumnDef, useReactTable } from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import { FunctionComponent } from "react";
 import { DataTableSelectableTableHeader } from "../ui/DataTableSelectableHeader";
 import { DataTableSelectableRow } from "../ui/DataTableSelectableRow";
 import { DataTableColumnHeader } from "../ui/DataTableHeader";
 import { DataTable } from "../ui/DataTable";
+import { Badge } from "../ui/Badge";
 
 export type Payment = {
   check?: undefined;
@@ -26,22 +27,41 @@ const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "id",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Task ID" />,
+    enableColumnFilter: true,
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
   },
   {
     accessorKey: "email",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Email" />,
     cell: ({ row }) => (
       <div className="flex items-center gap-2">
-        <span className="inline-flex items-center rounded-md capitalize border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground">
-          {row.original.status}
-        </span>
         <div className="max-w-[40rem] truncate">{row.original.email}</div>
       </div>
     ),
+    enableColumnFilter: false,
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => <DataTableColumnHeader column={column} title="Status" />,
+    cell: ({ row }) => (
+      <Badge variant={"secondary"} className="capitalize">
+        {row.original.status}
+      </Badge>
+    ),
+    enableColumnFilter: true,
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
   },
   {
     accessorKey: "amount",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Amount" />,
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+    //enableColumnFilter: false,
   },
 ];
 
@@ -52,9 +72,7 @@ type TicketsDataTableProps = {
 export const TicketsDataTable: FunctionComponent<TicketsDataTableProps> = ({ data }) => {
   return (
     <>
-      <DataTable name="tickets" data={data} columns={columns} />
-      <DataTable name="2" data={data} columns={columns} />
-      <DataTable name="3" data={data} columns={columns} />
+      <DataTable data={data} columns={columns} enableColumnVisibilityToggle={false} />
     </>
   );
 };
