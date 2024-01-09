aecc9ca8ee1096caae1784da675692c585af9cc6
Eric Sison <ericsison.dev@gmail.com>
2024-01-10 01:52:04 +0800

    added spacing between title and subtitle

--- src/components/features/PageHeading.tsx
+++ src/components/features/PageHeading.tsx
@@ -1,17 +1,18 @@
 import { FunctionComponent } from "react";
 import { Heading } from "../typography/Heading";
 import { Lead } from "../typography/Lead";
+import { Paragraph } from "../typography/Paragraph";
 
 type PageHeadingProps = {
   title: string;
-  subtitle: string;
+  subtitle?: string;
 };
 
 export const PageHeading: FunctionComponent<PageHeadingProps> = ({ title, subtitle }) => {
   return (
-    <div>
-      <Heading as="h2">{title}</Heading>
-      <Lead>{subtitle}</Lead>
+    <div className="space-y-1">
+      <Heading as="h3">{title}</Heading>
+      <Paragraph muted>{subtitle}</Paragraph>
     </div>
   );
 };
