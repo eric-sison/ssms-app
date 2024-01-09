6814381e7f764f01c58be73cfb8bd082478480f9
Eric Sison <ericsison.dev@gmail.com>
2024-01-10 01:55:59 +0800

    wrapped pages into page container component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,14 @@
 import { Sidebar } from "@/components/navigation/Sidebar";
 import { LayoutProps } from "../layout";
+import { PageContainer } from "@/components/features/PageContainer";
 
 export default function MainPageLayout({ children }: LayoutProps) {
   return (
     <div className="flex h-full">
       <Sidebar />
-      <main className="h-full w-full p-10 overflow-y-auto">{children}</main>
+      <main className="h-full w-full p-10 overflow-y-auto">
+        <PageContainer>{children}</PageContainer>
+      </main>
     </div>
   );
 }
