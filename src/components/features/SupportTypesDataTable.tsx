7cd2e74339a6193f305f7f3c616f36d062b3863d
Eric Sison <ericsison.dev@gmail.com>
2024-01-10 01:52:36 +0800

    added some mock data into the data table

--- src/components/features/SupportTypesDataTable.tsx
+++ src/components/features/SupportTypesDataTable.tsx
@@ -1,14 +1,30 @@
+"use client";
+
 import { FunctionComponent } from "react";
 import { DataTable } from "../ui/DataTable";
+import { SupportType } from "@/app/(main)/support-types/page";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "../ui/DataTableHeader";
 
-export const SupportTypesDataTable: FunctionComponent = () => {
-  return (
-    <DataTable
-      columns={[]}
-      data={[]}
-      enableColumnVisibilityToggle={false}
-      enablePagination={false}
-      enableGlobalFilter={false}
-    />
-  );
+type SupportTypesDataTableProps = {
+  data: SupportType[];
+};
+
+const columns: ColumnDef<SupportType>[] = [
+  {
+    accessorKey: "displayName",
+    header: ({ column }) => <DataTableColumnHeader title={"Name"} column={column} />,
+    cell: (info) => info.getValue(),
+    enableColumnFilter: false,
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => <DataTableColumnHeader title={"Description"} column={column} />,
+    cell: (info) => info.getValue(),
+    enableColumnFilter: false,
+  },
+];
+
+export const SupportTypesDataTable: FunctionComponent<SupportTypesDataTableProps> = ({ data }) => {
+  return <DataTable columns={columns} data={data} />;
 };
