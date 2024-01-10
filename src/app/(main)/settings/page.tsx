6346d647dd0c3d6aedbcea863409da1b2d532947
Eric Sison <ericsison.dev@gmail.com>
2024-01-10 12:19:07 +0800

    added test buttons for showing and hiding react-query-devtool in production environment

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/Button";
+import { useDevToolStore } from "@/lib/devtoolStore";
 import { useTheme } from "next-themes";
 
 export default function Settings() {
   const { setTheme } = useTheme();
+  const showDevTools = useDevToolStore((state) => state.setShowDevTools);
+
   return (
-    <div className="p-10 space-x-5">
+    <div className="space-x-5">
       <Button onClick={() => setTheme("light")}>Light</Button>
       <Button onClick={() => setTheme("dark")}>Dark</Button>
+
+      <Button onClick={() => showDevTools(true)}>Show Query Provider</Button>
+      <Button onClick={() => showDevTools(false)}>Hide Query Provider</Button>
     </div>
   );
 }
