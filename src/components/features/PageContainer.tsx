c942a1202a0e503fc49b7c92dc47705ba256dc7b
Eric Sison <ericsison.dev@gmail.com>
2024-01-10 12:15:31 +0800

    transfered padding inside PageContainer component

--- src/components/features/PageContainer.tsx
+++ src/components/features/PageContainer.tsx
@@ -3,15 +3,8 @@ import { HTMLAttributes, forwardRef } from "react";
 
 export const PageContainer = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => {
-    return <div ref={ref} className={cn("h-full space-y-8", className)} {...props} />;
+    return <div ref={ref} className={cn("h-full space-y-8 p-10", className)} {...props} />;
   }
 );
 
 PageContainer.displayName = "PageContainer";
-// return <div className="h-full space-y-8"></div>;
-
-{
-  /* <FormItemContext.Provider value={{ id }}>
-<div ref={ref} className={cn("space-y-2", className)} {...props} />
-</FormItemContext.Provider> */
-}
