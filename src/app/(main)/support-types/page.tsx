6895214fc6f32b8b64eb75a5a7a9b207a684ea6d
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 01:49:01 +0800

    removed props from SupportTypesDataTable component

--- src/app/(main)/support-types/page.tsx
+++ src/app/(main)/support-types/page.tsx
@@ -1,6 +1,6 @@
 import { SupportTypesDataTable } from "@/components/features/SupportTypesDataTable";
 import { AddSupportTypeModal } from "@/components/features/AddSupportTypeModal";
-import { BreadCrumbs, BreadCrumbsLink } from "@/components/ui/BreadCrumbs";
+import { BreadCrumbsLink } from "@/components/ui/BreadCrumbs";
 import { Hammer } from "lucide-react";
 import { Metadata } from "next";
 import { PageHead } from "@/components/features/PageHead";
@@ -10,41 +10,12 @@ export const metadata: Metadata = {
   title: "Support Types",
 };
 
-//TODO: remove this sample data
-export type SupportType = {
-  displayName: string;
-  description: string;
-  createdAt: Date;
-};
-
-const data: SupportType[] = [
-  {
-    displayName: "Simple Support",
-    description:
-      "Refers to a type of support that can be accomplished within three (3) working days from the receipt of the request.",
-    createdAt: new Date(),
-  },
-
-  {
-    displayName: "Complex Support",
-    description:
-      "Refers to a type of support that can be accomplished within ten (10) working days from the receipt of the request.",
-    createdAt: new Date(),
-  },
-
-  {
-    displayName: "Remote",
-    description: "Refers to a type of support that can be done virtually.",
-    createdAt: new Date(),
-  },
-];
-
 const links: BreadCrumbsLink[] = [
   { path: "/", label: "Home" },
   { path: "/support-types", label: "Support Types" },
 ];
 
-export default function SupportTypes() {
+export default async function SupportTypes() {
   return (
     <>
       <PageHead
@@ -59,7 +30,7 @@ export default function SupportTypes() {
       </PageHead>
 
       <PageBody>
-        <SupportTypesDataTable data={data} />
+        <SupportTypesDataTable />
       </PageBody>
     </>
   );
