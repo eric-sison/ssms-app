395932256494809d65a26d8d252589a3cd5ebf64
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 01:44:46 +0800

    removed temporary mock data and connected to backend api

--- src/components/features/SupportTypesDataTable.tsx
+++ src/components/features/SupportTypesDataTable.tsx
@@ -2,20 +2,21 @@
 
 import { FunctionComponent } from "react";
 import { DataTable } from "../ui/DataTable";
-import { SupportType } from "@/app/(main)/support-types/page";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "../ui/DataTableHeader";
-
-type SupportTypesDataTableProps = {
-  data: SupportType[];
-};
+import { SupportType } from "@/types/SupportType";
+import { useQuery } from "@tanstack/react-query";
+import { getAllSupportTypes } from "@/functions/http/support-types";
+import dayjs from "dayjs";
 
 const columns: ColumnDef<SupportType>[] = [
   {
-    accessorKey: "displayName",
+    accessorKey: "name",
     header: ({ column }) => <DataTableColumnHeader title={"Name"} column={column} />,
     cell: (info) => info.getValue(),
-    enableColumnFilter: false,
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
   },
   {
     accessorKey: "description",
@@ -23,8 +24,21 @@ const columns: ColumnDef<SupportType>[] = [
     cell: (info) => info.getValue(),
     enableColumnFilter: false,
   },
+  {
+    accessorKey: "updatedAt",
+    header: ({ column }) => <DataTableColumnHeader title="Last updated" column={column} />,
+    cell: (info) => <>{dayjs(info.getValue() as Date).format("dddd, MMMM D, YYYY")}</>,
+    enableColumnFilter: false,
+  },
 ];
 
-export const SupportTypesDataTable: FunctionComponent<SupportTypesDataTableProps> = ({ data }) => {
-  return <DataTable columns={columns} data={data} />;
+export const SupportTypesDataTable: FunctionComponent = () => {
+  const { data: result, isLoading } = useQuery({
+    queryKey: ["support-types"],
+    queryFn: getAllSupportTypes,
+  });
+
+  if (isLoading) return <>Loading...</>;
+
+  if (result) return <DataTable columns={columns} data={result.data} />;
 };
