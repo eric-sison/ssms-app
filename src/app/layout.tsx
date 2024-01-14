78874b1e18187d47500659129713d460525be4b0
Eric Sison <ericsison.dev@gmail.com>
2024-01-15 01:47:58 +0800

    added toaster component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { QueryProvider } from "@/components/providers/QueryProvider";
+import { Toaster } from "@/components/ui/Sonner";
 import "../styles/tailwind.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: LayoutProps) {
         >
           <QueryProvider>{children}</QueryProvider>
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
