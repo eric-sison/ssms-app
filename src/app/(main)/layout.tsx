df1758ec1d5390f94676c4d3762eec46d1105412
Eric Sison <ericsison.dev@gmail.com>
2024-01-19 16:03:24 +0800

    added functionality to redirect main pages to /login if refresh_token cookie is not set

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,23 @@
 import { Sidebar } from "@/components/navigation/Sidebar";
 import { LayoutProps } from "../layout";
 import { PageContainer } from "@/components/features/PageContainer";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { TokenProvider } from "@/components/providers/TokenProvider";
 
-export default function MainPageLayout({ children }: LayoutProps) {
+export default async function MainPageLayout({ children }: LayoutProps) {
+  if (!cookies().get("refresh_token")) {
+    redirect("/login");
+  }
+
   return (
-    <div className="flex h-full">
-      <Sidebar />
-      <main className="h-full w-full overflow-y-auto">
-        <PageContainer>{children}</PageContainer>
-      </main>
-    </div>
+    <TokenProvider>
+      <div className="flex h-full">
+        <Sidebar />
+        <main className="h-full w-full overflow-y-auto">
+          <PageContainer>{children}</PageContainer>
+        </main>
+      </div>
+    </TokenProvider>
   );
 }
