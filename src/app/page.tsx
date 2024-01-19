36856a1c450d2b767c88851702b1d116a2bc1786
Eric Sison <ericsison.dev@gmail.com>
2024-01-19 16:01:27 +0800

    fixed formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <main className="h-screen w-screen flex items-center justify-center">
       <div className="h-20 w-20 border-8 border-r-transparent animate-spin rounded-full" />
